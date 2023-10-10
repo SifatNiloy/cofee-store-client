0b993a63f7da612098f97644207c2be9aca489d1
SifatNiloy <sifatniloy18@gmail.com>
2023-10-10 12:57:44 +0600

    add coffee two form added

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const AddCoffee = () => {
-    return (
-        <div>
-            <h2>Add a coffee</h2>
+  return (
+    <div>
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="info@site.com"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="available quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
