8d9fe330799ef0a8babd68ff659c978146595d06
SifatNiloy <sifatniloy18@gmail.com>
2023-10-11 12:42:58 +0600

    coffee card updated

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
@@ -23,7 +24,7 @@ const CoffeeCard = ({ coffee }) => {
           .then((data) => {
             console.log(data);
             if (data.deletedCount > 0) {
-              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              Swal.fire("Deleted!", "Coffee has been deleted.", "success");
             }
           });
       }
@@ -46,7 +47,9 @@ const CoffeeCard = ({ coffee }) => {
         <div className="card-actions justify-end">
           <div className="btn-group btn-group-vertical">
             <button className="btn btn-active">View</button>
-            <button className="btn">Edit</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn">Edit</button>
+            </Link>
             <button onClick={() => handleDelete(_id)} className="btn">
               Delete
             </button>
