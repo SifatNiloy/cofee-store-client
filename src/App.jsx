c61f516752a65728e56003a38eacd069b2670570
SifatNiloy <sifatniloy18@gmail.com>
2023-11-29 18:33:20 +0600

    delte operation success

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { useLoaderData } from "react-router-dom";
 import CoffeeCard from "./components/CoffeeCard";
 
 function App() {
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
   return (
     <div className="m-20">
       <h1 className="text-6xl text-purple-600">
@@ -13,7 +14,12 @@ function App() {
       </h1>
       <div className="grid md:grid-cols-2 gap-4 ">
         {coffees.map((coffee) => (
-          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+          <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>
         ))}
       </div>
     </div>
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
-const CoffeeCard = ({ coffee }) => {
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
   const { _id, name, quantity, supplier, taste, catagory, details, photo } =
     coffee;
   const handleDelete = (_id) => {
@@ -25,6 +25,8 @@ const CoffeeCard = ({ coffee }) => {
             console.log(data);
             if (data.deletedCount > 0) {
               Swal.fire("Deleted!", "Coffee has been deleted.", "success");
+              const remaining = coffees.filter((cof) => cof._id != _id);
+              setCoffees(remaining);
             }
           });
       }
